10def1efca80909e0ee2d157f4bfe1258ac83f6f
Tommy Pettersson <tommypettersson04@gmail.com>
2022-12-29 16:29:25 +0100

    optimize imports

--- scorekeeper.py
+++ scorekeeper.py
@@ -1,16 +1,18 @@
-import pygame
+from pygame.sprite import Sprite
+from pygame.display import get_surface
+from pygame.font import SysFont
 
 
-class ScoreKeeper(pygame.sprite.Sprite):
+class ScoreKeeper(Sprite):
 
     def __init__(self, x, y):
         super().__init__()
-        screen = pygame.display.get_surface()
+        screen = get_surface()
         self.area = screen.get_rect()
 
         self.score = 0
 
-        self.font = pygame.font.SysFont('comicsans', 40)
+        self.font = SysFont('comicsans', 40)
         self.image = self.font.render(str(self.score), True, (255, 255, 255))
         self.image = self.image.convert_alpha()
         self.rect = self.image.get_rect(center=(x, y))
